55f272213e8383ef3d341cefa2034dac12a6e3b0
orayas1996 <ritzjane@gmail.com>
2017-11-02 14:38:53 +0700

    แก้นิดนึง

--- database/migrations/2017_11_02_063029_create_clubs_table.php
+++ database/migrations/2017_11_02_063029_create_clubs_table.php
@@ -14,7 +14,7 @@ class CreateClubsTable extends Migration
     public function up()
     {
         Schema::create('clubs', function (Blueprint $table) {
-            $table-string('culb_id');
+            $table->string('culb_id');
 			$table->string('name');
 			$table->string('time'); /**เวลาเรียน*/
             $table->timestamps();
