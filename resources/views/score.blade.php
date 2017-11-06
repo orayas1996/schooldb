12a93840ea45bad0f3fea12386d9dd313c0bc855
orayas1996 <ritzjane@gmail.com>
2017-11-06 19:15:52 +0700

    ตารางเกรด

--- resources/views/score.blade.php
+++ resources/views/score.blade.php
@@ -8,38 +8,39 @@
 <h4><center>ผลการเรียน</center></h4>
 <table width="723" height="495">
   <tr align="center" valign="middle">
-    <td height="38" colspan="2">&nbsp;</td>
+    <td height="38" align="center">  &nbsp;ชื่อ</td>
+    <td height="38" align="center">รหัสนักเรียน</td>
+  </tr>
+  <tr bgcolor="#CCCCCC">
+    <td width="372" height="26" align="center" valign="middle">วิชา</td>
+    <td width="339" align="center" valign="middle">เกรด</td>
   </tr>
   <tr>
-    <td height="26" align="center" valign="middle">&nbsp;</td>
-    <td align="center" valign="middle">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="center" valign="middle">&nbsp;</td>
-    <td align="center" valign="middle">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="center" valign="middle">&nbsp;</td>
+    <td align="center" valign="middle">คณิตศาสตร์</td>
     <td align="center" valign="middle">&nbsp;</td>
   </tr>
   <tr>
-    <td align="center" valign="middle">&nbsp;</td>
-    <td align="center" valign="middle">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="center" valign="middle">&nbsp;</td>
+    <td align="center" valign="middle">ภาษาไทย</td>
     <td align="center" valign="middle">&nbsp;</td>
   </tr>
   <tr>
-    <td align="center" valign="middle">&nbsp;</td>
-    <td align="center" valign="middle">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="center" valign="middle">&nbsp;</td>
+    <td align="center" valign="middle">วิทยาศาสตร์</td>
     <td align="center" valign="middle">&nbsp;</td>
   </tr>
   <tr>
+    <td align="center" valign="middle">สังคมศึกษาและวัฒนธรรม</td>
     <td align="center" valign="middle">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle">ภาษาอังกฤษ</td>
+    <td align="center" valign="middle">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle">สุขศึกษาและพละ</td>
+    <td align="center" valign="middle">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle">ศิลปะ</td>
     <td align="center" valign="middle">&nbsp;</td>
   </tr>
 </table>
