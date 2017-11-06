f2c45b0497c1d15226c1f24e135d4e8fbb78ad6d
orayas1996 <ritzjane@gmail.com>
2017-11-06 15:41:08 +0700

    เพิ่มหน้าเพจ

---
+++ resources/views/schedule.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="907" height="311" cellpadding="9">
+  <caption>
+    ตารางเรียน
+  </caption>
+  <tr>
+    <td width="57" align="center" valign="middle" bgcolor="#FF9999">วัน/เวลา</td>
+    <td width="73">&nbsp;</td>
+    <td width="85">&nbsp;</td>
+    <td width="83">&nbsp;</td>
+    <td width="81">&nbsp;</td>
+    <td width="77">&nbsp;</td>
+    <td width="88">&nbsp;</td>
+    <td width="87">&nbsp;</td>
+    <td width="92">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle" bgcolor="#FF9999">จันทร์</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle" bgcolor="#FF9999">อังคาร</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle" bgcolor="#FF9999">พุธ</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle" bgcolor="#FF9999">พฤหัส</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle" bgcolor="#FF9999">ศุกร์</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
