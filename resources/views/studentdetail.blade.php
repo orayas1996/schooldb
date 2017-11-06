6cc2bf58493308eadb96c21b36f89e1c56c147bf
orayas1996 <ritzjane@gmail.com>
2017-11-06 20:10:09 +0700

    ตารางนรครู

---
+++ resources/views/studentdetail.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>student detail</title>
+</head>
+<body>
+<h1><center>ข้อมูลนักเรียน</center></h1>
+
+<table width="724" height="544">
+  <tr align="right" valign="middle">
+    <td height="223" colspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="126" height="42" align="right" valign="middle">ชื่อ:</td>
+    <td width="586">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="33" align="right" valign="middle">รหัสนักเรียน:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="30" align="right" valign="middle">เลขรหัสประชาชน:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="40" align="right" valign="middle">เพศ:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="37" align="right" valign="middle">อายุ:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="39" align="right" valign="middle">ที่อยู่:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="39" align="right" valign="middle">ชั้นปี:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="39" align="right" valign="middle">ห้องเรียน:</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
+
+<body>
+</body>
+</html>
---
+++ resources/views/teacherdetail.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>teacher detail</title>
+</head>
+<body>
+<h1><center>ข้อมูลบุคลากร</center></h1>
+
+<table width="724" height="530">
+  <tr align="right" valign="middle">
+    <td height="269" colspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="126" height="42" align="right" valign="middle">ชื่อ:</td>
+    <td width="586">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="33" align="right" valign="middle">เลขรหัสประชาชน:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="30" align="right" valign="middle">เพศ:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="40" align="right" valign="middle">ที่อยู่:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="37" align="right" valign="middle">ห้องพักครู:</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="39" align="right" valign="middle">ที่ปรึกษาชมรม:</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
+
+<body>
+</body>
+</html>
