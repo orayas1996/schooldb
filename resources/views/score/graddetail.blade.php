d2ba119b7441eeeb90a3893a5913f3355b1b06e1
Osassssss <33480011+Osassssss@users.noreply.github.com>
2017-12-12 13:37:32 +0700

    version0.2

--- resources/views/score/graddetail.blade.php
+++ resources/views/score/graddetail.blade.php
@@ -1,120 +1,153 @@
-@extends('layouts.app')
 <!DOCTYPE html>
-@section('content')
+<html>
+<head>
+<title>student detail</title>
+</head>
+<body>
+<h1><center>ผลการเรียน</center></h1>
+<center>
+  <tr>
+    <td width="126" height="42" align="center" valign="middle">ชื่อ:</td>
+    <td width="300">&nbsp;{{$student->name}}</td>
+  </tr>
 
-
-<center><div class="page-header ">
-<h2>ผลการเรียน</h2>
-</div>
-<table>
+  <table border=1>
   <tr>
-    <td width="30" height="42" align="center" valign="middle">ชื่อ:</td>
-    <td>&nbsp;{{$student->name}}</td>
-  </tr>
-</table>
-<br>
-  <table class="table table-striped" border=2>
-  <tr>
-    <td align="center" valign="middle" rowspan="7" width="200">มัธยมศึกษาปีที่ 1</td>
-	<th align="center" valign="middle" width="150" bgcolor="lightgray"><center>รายวิชา
-	</center></th>
-	<th align="center" valign="middle" bgcolor="lightgray"><center>ผลการเรียน
-    </center></th>	
+    <td height="40" align="center" valign="middle">มัธยมศึกษาปีที่ 1</td>
+	<th  align="center" colspan="1"    valign="middle">รายวิชา
+	</th>
+	<th  align="center"  rowspan="1"   valign="middle">ผลการเรียน
+    </th>	
   </tr>
   <tr>
-  <td><center>ภาษาไทย</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ภาษาไทย</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>คณิตศาสตร์</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>คณิตศาสตร์</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>ภาษาอังกฤษ</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ภาษาอังกฤษ</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>สังคม</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>สังคม</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>วิทยาศาสตร์</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>วิทยาศาสตร์</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>ศิลปะ</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ศิลปะ</td>
+  <td>A</td>
 </tr>
-  <tr>
-    <td align="center" valign="middle" rowspan="7" width="200">มัธยมศึกษาปีที่ 2</td>
-	<th align="center" valign="middle" width="150" bgcolor="lightgray"><center>รายวิชา
-	</center></th>
-	<th align="center" valign="middle" bgcolor="lightgray"><center>ผลการเรียน
-    </center></th>	
+<tr>
+  <td>เกรดเฉลี่ยรวม</td>
+  <td></td>
+  <td>4.00</td>
+</tr>
+<tr>
+    <td height="40" align="center" valign="middle">มัธยมศึกษาปีที่ 2</td>
+	<th  align="center" colspan="1"    valign="middle">รายวิชา
+	</th>
+	<th  align="center"  rowspan="1"   valign="middle">ผลการเรียน
+    </th>	
   </tr>
   <tr>
-  <td><center>ภาษาไทย</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ภาษาไทย</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>คณิตศาสตร์</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>คณิตศาสตร์</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>ภาษาอังกฤษ</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ภาษาอังกฤษ</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>สังคม</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>สังคม</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>วิทยาศาสตร์</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>วิทยาศาสตร์</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>ศิลปะ</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ศิลปะ</td>
+  <td>A</td>
 </tr>
-  <tr>
-    <td align="center" valign="middle" rowspan="7" width="200">มัธยมศึกษาปีที่ 3</td>
-	<th align="center" valign="middle" width="150" bgcolor="lightgray"><center>รายวิชา
-	</center></th>
-	<th align="center" valign="middle" bgcolor="lightgray"><center>ผลการเรียน
-    </center></th>	
+<tr>
+  <td>เกรดเฉลี่ยรวม</td>
+  <td></td>
+  <td>4.00</td>
+</tr>
+<tr>
+    <td height="40" align="center" valign="middle">มัธยมศึกษาปีที่ 3</td>
+	<th  align="center" colspan="1"    valign="middle">รายวิชา
+	</th>
+	<th  align="center"  rowspan="1"   valign="middle">ผลการเรียน
+    </th>	
   </tr>
   <tr>
-  <td><center>ภาษาไทย</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ภาษาไทย</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>คณิตศาสตร์</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>คณิตศาสตร์</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>ภาษาอังกฤษ</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ภาษาอังกฤษ</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>สังคม</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>สังคม</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>วิทยาศาสตร์</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>วิทยาศาสตร์</td>
+  <td>A</td>
 </tr>
 <tr>
-  <td><center>ศิลปะ</center></td>
-  <td><center>A</center></td>
+  <td></td>
+  <td>ศิลปะ</td>
+  <td>A</td>
+</tr>
+<tr>
+  <td>เกรดเฉลี่ยรวม</td>
+  <td></td>
+  <td>4.00</td>
 </tr>
 </table>
 </center>
 
 <br>
 <center>
-<form action="/score">
-    <input class="btn" type="submit" value="กลับ">
+<form action="/students/searchstudent">
+    <input type="submit" value="Back">
 	</form>
 </center>
 
-@endsection
+<body>
+</body>
+</html>
